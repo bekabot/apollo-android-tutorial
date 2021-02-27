8d15a50b6e341c79b16a44e2db60e8874e44fece
bekabot <bekabot@mail.ru>
2021-02-27 19:50:41 +0600

    reproduce crash with custom interceptor

--- app/src/main/java/com/example/rocketreserver/Apollo.kt
+++ app/src/main/java/com/example/rocketreserver/Apollo.kt
@@ -2,11 +2,18 @@ package com.example.rocketreserver
 
 import android.content.Context
 import android.os.Looper
+import android.util.Log
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.cache.http.ApolloHttpCache
+import com.apollographql.apollo.cache.http.DiskLruHttpCacheStore
 import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
+import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
+import java.io.File
+import java.io.IOException
 
 private var instance: ApolloClient? = null
 
@@ -21,10 +28,21 @@ fun apolloClient(context: Context): ApolloClient {
 
     val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(AuthorizationInterceptor(context))
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .addInterceptor(TestInterceptor())
         .build()
+    // Directory where cached responses will be stored
+    val file = File(context.cacheDir, "apolloCache")
+
+    // Size in bytes of the cache
+    val size: Long = 1024 * 1024 * 10
+    val cacheStore = DiskLruHttpCacheStore(file, size)
 
     instance = ApolloClient.builder()
         .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
+        .httpCache(ApolloHttpCache(cacheStore))
         .subscriptionTransportFactory(WebSocketSubscriptionTransport.Factory("wss://apollo-fullstack-tutorial.herokuapp.com/graphql", okHttpClient))
         .okHttpClient(okHttpClient)
         .build()
@@ -39,5 +57,18 @@ private class AuthorizationInterceptor(val context: Context) : Interceptor {
             .build()
 
         return chain.proceed(request)
+    }
+}
+
+private class TestInterceptor : Interceptor {
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val originalResponse = chain.proceed(chain.request())
+        val responseBodyCopy: ResponseBody? = originalResponse.peekBody(Long.MAX_VALUE)
+        Log.d("TEST_INTERCEPTOR", responseBodyCopy?.string().orEmpty())
+
+        return originalResponse
     }
 }
